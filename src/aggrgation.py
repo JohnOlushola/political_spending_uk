aa6de860624e3237b875009e327098d30f122258
Isaac Akin-Onigbinde <aakinonigbinde@gmail.com>
2022-07-25 00:23:29 +0100

    aggregation_func

---
+++ src/aggrgation.py
@@ -0,0 +1,12 @@
+def aggregate_items(original_id, description, price, custom_label):
+  aggregate_array = []
+  for item in data_items:
+    if original_id==original_id and custom_label==custom_label:
+      aggregate_array.append(id, sum, custom_label)
+
+  return aggregate_array
+
+
+# class BreadSticks(self, original_id, description, price, custom_label):
+#   def __init__():
+#     pass
